d89fdccc21212fc8c729930bd6760e492e6a07ba
shazab <shahzeb61518@gmail.com>
2021-04-16 17:39:08 +0500

    cahnges commit

--- server.js
+++ server.js
@@ -16,20 +16,20 @@ mongoose.connect(url, (err, db) => {
   console.log("DB is Connected");
 });
 
-app.use(cors())
-// app.use(function (req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,Content-Type,Content-Length,Host,Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
+// app.use(cors())
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type,Content-Length,Host,Authorization,Connection"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use(bodyParser.json({ limit: "5000mb" }));
 app.use(bodyParser.urlencoded({ extended: false, limit: "5000mb" }));
